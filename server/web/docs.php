552acca6912a36f33f8abade6344f075747c784f
ted.eriksson@me.com <ted.eriksson@me.com>
2013-10-27 20:40:06 +0000

    added homepage and docs section, started to finalize JSON format

---
+++ server/web/docs.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Poi Docs</title>
+	<link rel="stylesheet" type="text/css" href="/web/main.css">
+</head>
+<body>
+	<header>
+		<h1>Documentation</h1>
+	</header>
+	<nav>
+		<h2>Navigation</h2>
+		<ul>
+			<li>JSON Output</li>
+		</ul>	
+	</nav>
+	<div class="block light">
+		<h2>Json output</h2>
+		<h3>GET Requests</h3>
+		<p>
+		<h4>An Example for getting a point by ID</h4>
+			<pre class="url">api/points/#</pre>
+			<pre>
+"points": [{
+	"id": "1",
+	"name":	"Object",
+	"lng": "45",
+	"lat": "75",
+	"message": "This is a general description of the object"
+	"nodes": [{
+		"x": 33,
+		"y": -17,
+		"z": 20,
+		"message": "This is an interesting part of the object"			
+		}]
+	}]
+			</pre>
+		</p>
+	</div>
+	<div class="block dark">
+		<h2>Json output</h2>
+		<h3>GET Requests</h3>
+		<p>
+		<h4>An Example for getting a point by ID</h4>
+			<pre>api/points/#</pre>
+			<pre>
+"points": [{
+	"id": "1",
+	"name":	"Object",
+	"lng": "45",
+	"lat": "75",
+	"message": "This is a general description of the object"
+	"nodes": [{
+		"x": 33,
+		"y": -17,
+		"z": 20,
+		"message": "This is an interesting part of the object"			
+		}]
+	}]
+			</pre>
+		</p>
+	</div>
+	<div class="block light">
+		<h2>Json output</h2>
+		<h3>GET Requests</h3>
+		<p>
+		<h4>An Example for getting a point by ID</h4>
+			<pre>api/points/#</pre>
+			<pre>
+"points": [{
+	"id": "1",
+	"name":	"Object",
+	"lng": "45",
+	"lat": "75",
+	"message": "This is a general description of the object"
+	"nodes": [{
+		"x": 33,
+		"y": -17,
+		"z": 20,
+		"message": "This is an interesting part of the object"			
+		}]
+	}]
+			</pre>
+		</p>
+	</div>
+</body>
+</html>
--- server/web/index.php
+++ server/web/index.php
@@ -4,8 +4,20 @@
 	<title>Points of interest</title>
 	<link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon">
 	<link rel="icon" href="/favicon.ico?v=2" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="/web/main.css">
 </head>
-<body>
-	<h1>Points of interest</h1>
+<body class="homepage">
+	<div class="logo"></div>
+	<ul class="links">
+		<li>
+			<a href="/api/">Api</a>
+		</li>
+		<li>
+			<a href="/docs/">Docs</a>
+		</li>
+		<li>
+			<a href="/about/">About</a>
+		</li>
+	</ul>
 </body>
 </html>
