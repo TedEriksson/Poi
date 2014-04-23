1c8b4109cf3dc63a13b053e8df1e0ce0877e6d3b
Ted Eriksson <ted.eriksson@me.com>
2014-04-23 14:30:10 +0000

    remove old

--- server/api/connectinfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('DATABASE_HOSTNAME', '79.170.44.111:3306');
-	define('DATABASE_USER', 'web111-poiadmin');
-	define('DATABASE_PASSWORD', '7q7Fhg/gD');
-	define('DATABASE_DATABASE', 'web111-poiadmin');
-?>
